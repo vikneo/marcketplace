e226801cb52ae5965032aa9a563db6d0daef7746
viktor <viktor-vm@mail.ru>
2023-11-02 22:42:41 +0700

    added model user and register to admin panel

--- topline/users/models.py
+++ topline/users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    sale = models.IntegerField()
+    avatar = models.ImageField()
+    name_store = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    viewed_orders = models.ForeignKey('store.Product', on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, default='user')
+
+    class Meta:
+        db_table = 'Profiles'
+        ordering = ['id', 'user']
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+        permissions = (
+            ('store', 'has offers for sale'),
+            ('user', 'all site users'),
+            ('buyer', 'only buys goods'),
+            ('admin', 'manages the site'),
+        )
