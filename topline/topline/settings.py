e063a8d3a872e18c34347bd372847bee799e7576
viktor <viktor-vm@mail.ru>
2023-11-02 22:43:18 +0700

    added app users in INSTALLED_APPS

--- topline/topline/settings.py
+++ topline/topline/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'store.apps.StoreConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- topline/topline/urls.py
+++ topline/topline/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('user/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
